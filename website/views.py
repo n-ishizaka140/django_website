083c44f919aa50425bf843ee861f3d1b3856b2c6
n_ishizaka140 <VFFproduction@gmail.com>
2020-11-10 18:27:43 +0900

    first commit

---
+++ website/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self):
+        ctxt = super().get_context_data()
+        ctxt["username"] = "Naoto"
+        return ctxt
+    
+class AboutView(TemplateView):
+    template_name = "about.html"
+
+    def get_context_data(self):
+        ctxt = super().get_context_data()
+        ctxt["num_services"] = 1234567
+        ctxt["skills"] = [
+            "Python",
+            "Ruby",
+            "Java",
+        ]
+        return ctxt
